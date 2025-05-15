df2e19fdce3067b2dcd5417125f68f04054e9705
Alexandre <alevaristofig@gmail.com>
2025-05-15 17:58:53 -0300

    Implementação da tarefa agendada para enviar um relatorio diario por email

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Mail;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('relatorio', function () {
+   
+    $result = DB::table('pedidos')
+        ->select('status', DB::raw('COUNT(*) as total'))
+        ->where('created_at', '>=', now()->subDays(7))
+        ->groupBy('status')
+        ->get();
+
+    $msg = "";
+
+    foreach($result as $r) {
+        $msg.= "Foram feitos ".$r->total. " de pedidos com status ".$r->status."<br>";        
+    }
+    
+     Mail::raw($msg, function ($message) {
+        $message->from('figalexandre@gmail.com', 'Alexandre');
+        $message->sender('alevaristofig@gmail.com', 'Alexandre');
+        $message->to('alevaristofig@gmail.com', 'Alexandre');
+});
+})->purpose('Relatorio');
+
+app(Schedule::class)->command('relatorio')->dailyAt('08:00');
